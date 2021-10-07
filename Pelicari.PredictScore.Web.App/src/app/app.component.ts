8e529195396706b5f943541ecbc9ff46a9ea00c3
Wilton Pelicari <willpelicari@gmail.com>
2021-10-07 09:25:51 -0400

    Bootstrap install + Angular hello world

--- Pelicari.PredictScore.Web.App/src/app/app.component.ts
+++ Pelicari.PredictScore.Web.App/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pelicari-PredictScore-Web-App';
+  title = "Peliçari's Predict Score";
 }
